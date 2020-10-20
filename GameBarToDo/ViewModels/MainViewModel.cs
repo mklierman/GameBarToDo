fe4c98ee1acf6b30fdfbd3e362c353ea927570a5
Mark Lierman <marklierman@hotmail.com>
2020-10-19 22:19:50 -0500

    Add project files.

---
+++ GameBarToDo/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using GameBarToDo.Helpers;
+
+namespace GameBarToDo.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
